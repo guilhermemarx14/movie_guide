abc4be95dcd61d0a47eb059917cc378bd578cec2
guilhermemarx14 <guilhermemarx14@gmail.com>
2021-07-18 00:45:36 -0300

    Caches

--- lib/controller/detailed_movie_controller.dart
+++ lib/controller/detailed_movie_controller.dart
@@ -1,16 +1,30 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movie_guide/model/detailed_movie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailedMovieController {
   static Future<DetailedMovie> getMovieById(int id) async {
+    //Verify if the value is already saved
+    final prefs = await SharedPreferences.getInstance();
+    var saved = prefs.getString('detailed_movie_${id}') ?? '';
+    if (saved.isNotEmpty) {
+      DetailedMovie? myMovie;
+      var json = jsonDecode(saved);
+
+      return DetailedMovie.fromJson(jsonDecode(saved));
+    }
+
     final response = await http.get(Uri.parse(
         'https://desafio-mobile.nyc3.digitaloceanspaces.com/movies/' +
             id.toString()));
     if (response.statusCode == 200) {
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('detailed_movie_${id}', response.body);
+
       return DetailedMovie.fromJson(jsonDecode(response.body));
     } else {
-      //keeps trying till success
+      //keeps on trying till success
       return getMovieById(id);
     }
   }
--- lib/controller/simplified_movie_controller.dart
+++ lib/controller/simplified_movie_controller.dart
@@ -1,20 +1,40 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:movie_guide/model/simplified_movie.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SimplifiedMovieController {
   static Future<List<SimplifiedMovie>> getMovies() async {
+    //Verify if the value is already saved
+    final prefs = await SharedPreferences.getInstance();
+    var saved = prefs.getString('simplified_movies_list') ?? '';
+    if (saved.isNotEmpty) {
+      List<SimplifiedMovie> myList = [];
+      var json = jsonDecode(saved);
+
+      for (var value in json) {
+        myList.add(SimplifiedMovie.fromJson(value));
+      }
+      return myList;
+    }
+
     final response = await http.get(
         Uri.parse('https://desafio-mobile.nyc3.digitaloceanspaces.com/movies'));
+
     if (response.statusCode == 200) {
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('simplified_movies_list', response.body);
+
       List<SimplifiedMovie> myList = [];
       var json = jsonDecode(response.body);
 
-      for (var value in json) myList.add(SimplifiedMovie.fromJson(value));
+      for (var value in json) {
+        myList.add(SimplifiedMovie.fromJson(value));
+      }
 
       return myList;
     } else {
-      //keeps trying till success
+      //keeps on trying till success
       return getMovies();
     }
   }
